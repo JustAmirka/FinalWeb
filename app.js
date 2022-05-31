379e79bf0475a6c561e50cb08d5695538ac63889
JustAmirka <97117667+JustAmirka@users.noreply.github.com>
2022-05-31 22:12:39 +0600

    123

--- app.js
+++ app.js
@@ -160,10 +160,8 @@ app.use(function (err, req, res, next) {
 
 
 
-var port = process.env.PORT || 3000;
-app.set("port", port);
-app.listen(port, () => {
-    console.log("Server running at port " + port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 module.exports = app;
